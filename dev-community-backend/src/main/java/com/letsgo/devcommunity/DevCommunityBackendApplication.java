511f036542644a51875730c828ef8741a91b52fb
KangNaewon <kang.naewon125@gmail.com>
2025-03-30 18:14:34 +0900

    add initial spring boot project

---
+++ dev-community-backend/src/main/java/com/letsgo/devcommunity/DevCommunityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.letsgo.devcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevCommunityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevCommunityBackendApplication.class, args);
+	}
+
+}
