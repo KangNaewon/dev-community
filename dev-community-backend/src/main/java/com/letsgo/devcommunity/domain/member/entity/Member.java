e448fb36386a026eb02cfa1f6dacb0e46db10acc
KangNaewon <kang.naewon125@gmail.com>
2025-04-01 21:14:22 +0900

    feat: Member 엔티티 생성

---
+++ dev-community-backend/src/main/java/com/letsgo/devcommunity/domain/member/entity/Member.java
@@ -0,0 +1,27 @@
+package com.letsgo.devcommunity.domain.member.entity;
+
+import com.letsgo.devcommunity.global.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String loginId;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+}
