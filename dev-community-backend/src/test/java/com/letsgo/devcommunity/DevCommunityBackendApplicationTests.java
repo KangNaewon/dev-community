81679724b67559a570c8972e5a968b64b3b007a1
KangNaewon <kang.naewon125@gmail.com>
2025-06-15 20:44:49 +0900

    Fix: Test 환경설정

--- dev-community-backend/src/test/java/com/letsgo/devcommunity/DevCommunityBackendApplicationTests.java
+++ dev-community-backend/src/test/java/com/letsgo/devcommunity/DevCommunityBackendApplicationTests.java
@@ -2,9 +2,18 @@ package com.letsgo.devcommunity;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @SpringBootTest
 class DevCommunityBackendApplicationTests {
+
+	@MockBean
+	private JavaMailSender javaMailSender;
+
+	@MockBean
+	private S3Client s3Client;
 
 	@Test
 	void contextLoads() {
